fc7c9f47259cac7f019bfc117d84436429443f9a
100PBaguuette <magnat.gregory@gmail.com>
2023-11-13 17:50:49 +0100

    1756

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    get '/users/sign_out', to: 'devise/sessions#destroy'
+   get '/users/sign_out', to: 'devise/sessions#destroy'
   end
 
   root to: 'events#index'
 
+  resources :users
   resources :attendances
   resources :events
   
